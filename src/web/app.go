2de73ca83d430d0c9976bfd009b7911359828127
Marius Orcsik <marius@habarnam.ro>
2018-06-10 17:45:41 +0200

    Using gorilla and gothic for oauth2 with github

--- src/web/app.go
+++ src/web/app.go
@@ -1,82 +1,120 @@
 package main
 
 import (
-	"github.com/kataras/iris"
-	"github.com/stretchr/gomniauth"
-	"github.com/stretchr/objx"
+	"html/template"
+	"net/http"
+	"os"
+
+	"sort"
+
+	"log"
+
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/github"
+	"github.com/gorilla/mux"
+	"github.com/michaeljs1990/sqlitestore"
 )
 
-func index(ctx iris.Context) {
-	// finally, render the template.
-	ctx.View("index.html")
+func logMw(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		log.Println(r.RequestURI)
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
 }
-
-func login(ctx iris.Context) {
-	provider, err := gomniauth.Provider(ctx.Params().Get("provider"))
-
-	if err != nil {
-		//
-	}
-
-	state := gomniauth.NewState("after", "success")
-
-	// if you want to request additional scopes from the provider,
-	// pass them as login?scope=scope1,scope2
-	//options := objx.MSI("scope", ctx.QueryValue("scope"))
-
-	authUrl, err := provider.GetBeginAuthURL(state, nil)
-
-	if err != nil {
-		//
-	}
-
-	// redirect
-	ctx.Redirect(authUrl) //goweb.Respond.WithRedirect(ctx, authUrl)
+func authMw(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+	})
 }
-
-func complete(ctx iris.Context) {
-	provider, err := gomniauth.Provider(ctx.Params().Get("provider"))
-
-	if err != nil {
-		//
-	}
-	m := objx.MSI(ctx.URLParams())
-	creds, err := provider.CompleteAuth(m)
-
-	if err != nil {
-		//
-	}
-
-	// load the user
-	user, userErr := provider.GetUser(creds)
-
-	if userErr != nil {
-		//
-	}
-
-	ctx.JSON(user)
-}
-
-// Serve using a host:port form.
-var addr = iris.Addr("localhost:3000")
 
 func main() {
-	app := iris.New()
-	// Register the templates/**.html as django and reload them on each request
-	// so changes can be reflected, set to false on production.
-	app.RegisterView(iris.Django("./templates", ".html").Reload(true))
+	goth.UseProviders(
+		//twitter.New(os.Getenv("TWITTER_KEY"), os.Getenv("TWITTER_SECRET"), "http://localhost:3000/auth/twitter/callback"),
+		// If you'd like to use authenticate instead of authorize in Twitter provider, use this instead.
+		//twitter.NewAuthenticate(os.Getenv("TWITTER_KEY"), os.Getenv("TWITTER_SECRET"), "http://localhost:3000/auth/twitter/callback"),
 
-	// GET: http://localhost:3000
-	app.Get("/", index)
-	app.Get("/auth/{provider:string}/login", login)
-	app.Get("/auth/{provider:string}/callback", complete)
+		//facebook.New(os.Getenv("FACEBOOK_KEY"), os.Getenv("FACEBOOK_SECRET"), "http://localhost:3000/auth/facebook/callback"),
+		//gplus.New(os.Getenv("GPLUS_KEY"), os.Getenv("GPLUS_SECRET"), "http://localhost:3000/auth/gplus/callback"),
+		github.New(os.Getenv("GITHUB_KEY"), os.Getenv("GITHUB_SECRET"), "http://localhost:3000/auth/github/callback"),
+		//gitlab.New(os.Getenv("GITLAB_KEY"), os.Getenv("GITLAB_SECRET"), "http://localhost:3000/auth/gitlab/callback"),
+	)
 
-	app.OnAnyErrorCode(func(ctx iris.Context) {
-		ctx.ViewData("Message", ctx.Values().GetStringDefault("message", "The page you're looking for doesn't exist"))
-		ctx.View("error.html")
+	m := make(map[string]string)
+	//m["facebook"] = "Facebook"
+	//m["twitter"] = "Twitter"
+	m["github"] = "Github"
+	//m["gitlab"] = "Gitlab"
+	//m["gplus"] = "Google Plus"
+
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	key := []byte("test!2#")
+	maxAge := 86400 * 30  // 30 days
+	isProd := false       // Set to true when serving over https
+
+	store, err := sqlitestore.NewSqliteStore("../../.cache/feeds.db", "sessions", "/", maxAge, key)
+	if err != nil {
+		panic(err)
+	}
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true   // HttpOnly should always be enabled
+	store.Options.Secure = isProd
+
+	gothic.Store = store
+
+	sort.Strings(keys)
+	providerIndex := &ProviderIndex{Providers: keys, ProvidersMap: m}
+
+	r := mux.NewRouter()
+	r.Use(logMw)
+	r.Use(authMw)
+
+	r.HandleFunc("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
+		user, err := gothic.CompleteUserAuth(res, req)
+		if err != nil {
+			t, _ := template.New("error.html").ParseFiles("templates/error.html")
+			t.Execute(res, err)
+			return
+		}
+		t, _ := template.New("user.html").ParseFiles("templates/user.html")
+		t.Execute(res, user)
 	})
 
-	// Now listening on: http://localhost:3000
-	// Application started. Press CTRL+C to shut down.
-	app.Run(addr)
+	r.HandleFunc("/logout/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		gothic.Logout(res, req)
+		res.Header().Set("Location", "/")
+		res.WriteHeader(http.StatusTemporaryRedirect)
+	})
+
+	r.HandleFunc("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		// try to get the user without re-authenticating
+		if gothUser, err := gothic.CompleteUserAuth(res, req); err == nil {
+			t, _ := template.New("user.html").ParseFiles("templates/user.html")
+			t.Execute(res, gothUser)
+			return
+		}
+		gothic.BeginAuthHandler(res, req)
+	})
+
+	r.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		t, err := template.New("index.html").ParseFiles("templates/index.html")
+		if err != nil {
+			te, _ := template.New("error.html").ParseFiles("templates/error.html")
+			te.Execute(res, err)
+			return
+		}
+		t.Execute(res, providerIndex)
+	})
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
+
+type ProviderIndex struct {
+	Providers    []string
+	ProvidersMap map[string]string
 }
