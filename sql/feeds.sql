543d8978b676b1f1840fdf1b92a9d1003e4543fd
Marius Orcsik <marius@habarnam.ro>
2020-07-25 21:24:32 +0200

    Added unsong to the list of feeds

--- sql/feeds.sql
+++ sql/feeds.sql
@@ -1,3 +1,4 @@
 insert into feeds (url, title, author) values ('https://www.parahumans.net/feed/', 'Ward', 'Wildbow');
 insert into feeds (url, title, author) values ('https://www.royalroad.com/syndication/25225', 'Delve', 'SenescentSoul');
 insert into feeds (url, title, author) values ('https://pyrebound.wordpress.com/feed/', 'Pyrebound', 'theredsheep');
+insert into feeds (url, title, author) values ('https://unsongbook.com/feed/', 'Unsong', 'Scott Alexander');
