d126241083e8d70fadb3dc26d4bd7273f327857e
marius <marius@littr.me>
2020-07-26 20:59:29 +0200

    Added Mother of Learning links

--- sql/feeds.sql
+++ sql/feeds.sql
@@ -2,3 +2,4 @@ insert into feeds (url, title, author) values ('https://www.parahumans.net/feed/
 insert into feeds (url, title, author) values ('https://www.royalroad.com/syndication/25225', 'Delve', 'SenescentSoul');
 insert into feeds (url, title, author) values ('https://pyrebound.wordpress.com/feed/', 'Pyrebound', 'theredsheep');
 insert into feeds (url, title, author) values ('https://unsongbook.com/feed/', 'Unsong', 'Scott Alexander');
+insert into feeds (title, author) values ('Mother of Learning', 'Nobody103');
