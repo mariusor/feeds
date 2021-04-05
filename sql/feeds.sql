5847dda33364cf9b9d721fef9c726ad74c1dc271
Marius Orcsik <marius@habarnam.ro>
2021-04-05 19:07:02 +0200

    Removing feeds for finished web-series

--- sql/feeds.sql
+++ sql/feeds.sql
@@ -1,7 +1,7 @@
-insert into feeds (url, title, author) values ('https://www.parahumans.net/feed/', 'Ward', 'Wildbow');
+insert into feeds (title, author) values ('Ward', 'Wildbow');
 insert into feeds (url, title, author, frequency) values ('https://www.royalroad.com/syndication/25225', 'Delve', 'SenescentSoul', 84600);
 insert into feeds (url, title, author, frequency) values ('https://pyrebound.wordpress.com/feed/', 'Pyrebound', 'theredsheep', 84600);
-insert into feeds (url, title, author) values ('https://unsongbook.com/feed/', 'Unsong', 'Scott Alexander');
+insert into feeds (title, author) values ('Unsong', 'Scott Alexander');
 insert into feeds (title, author) values ('Mother of Learning', 'Nobody103');
 insert into feeds (url, title, author, frequency) values ('https://palewebserial.wordpress.com/feed/','Pale', 'Wildbow', 84600);
 insert into feeds (url, title, author, frequency) values ('https://wanderinginn.wordpress.com/feed/', 'The Wandering Inn', 'PirateAba', 84600);
