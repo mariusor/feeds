a4494b4033247af0358b9dbe8e831a03298d545e
Marius Orcsik <marius@habarnam.ro>
2021-07-16 20:57:54 +0200

    Added 'The Flower That Bloomed Nowhere'

--- sql/feeds.sql
+++ sql/feeds.sql
@@ -5,3 +5,4 @@ insert into feeds (title, author) values ('Unsong', 'Scott Alexander');
 insert into feeds (title, author) values ('Mother of Learning', 'Nobody103');
 insert into feeds (url, title, author, frequency) values ('https://palewebserial.wordpress.com/feed/','Pale', 'Wildbow', 84600);
 insert into feeds (url, title, author, frequency) values ('https://wanderinginn.wordpress.com/feed/', 'The Wandering Inn', 'PirateAba', 84600);
+insert into feeds (url, title, author, frequency) values ('https://www.royalroad.com/syndication/28806', 'The Flower that Bloomed Nowhere', 'Lurina', 84600);
